79fd72a9c14b7a23abfc506a7b2832127305e768
ImAfif <afifadelayoub@gmail.com>
2021-11-03 19:04:00 +0000

    msg

---
+++ even.js
@@ -0,0 +1,13 @@
+const isEven = function (num){
+  return num % 2 === 0;
+}
+
+console.log(isEven(40))
+
+const exampleone = isEven(10)
+
+const exampletwo = isEven(25)
+
+console.log(exampleone)
+
+console.log(exampletwo)
