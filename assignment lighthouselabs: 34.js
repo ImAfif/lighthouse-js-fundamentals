092c2fb756d0da68418f44646a1223bb7977b68e
ImAfif <afifadelayoub@gmail.com>
2021-11-03 09:51:25 +0000

    Use a while loop to repeat a chorus line

--- assignment lighthouselabs: 34.js
+++ assignment lighthouselabs: 34.js
@@ -1,10 +1,13 @@
 const whichSchool  = function (age) {
+// Your code in here ...
   if (age < 13 ){
     return "Elementary School"
-} else if (age >= 13 && age <= 17){
+  } else if (age >= 13 && age <= 17) {
     return "Secondary School"
-} else if (age >= 18){
+  } else if (age >= 18){
     return "Lighthouse Labs"
   }
 }
 console.log(whichSchool(12))
+
+
