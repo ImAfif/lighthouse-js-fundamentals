f99b30e617c5f083da7e764774344f4d81dd101a
ImAfif <afifadelayoub@gmail.com>
2021-11-05 14:20:23 +0000

    random code

---
+++ ice-cream.js
@@ -0,0 +1,27 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer")
+
+console.log(iceCreamFlavours)
+console.log(iceCreamFlavours[1])
+
+
+console.log(iceCreamFlavours)
+
+console.log("g" || "a")
+
+console.log('khu\nrram')
+
+console.log(5 + "3")
+
+let a = 3;
+let b = 4;
+b = a;
+console.log(b);
+
+const areMultiples  = function (bigNumber, smallNumber) {
+  return bigNumber % smallNumber === 0;
+}
+
+console.log(areMultiples(15, 4));
